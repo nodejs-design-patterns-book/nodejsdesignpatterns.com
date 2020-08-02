18c842fe6a8d5ce0dee5cb655683a2b26170a157
Luciano <lucianomammino@gmail.com>
2020-08-02 20:54:26 +0200

    Better sass setup, better responsive images

--- .eleventy.js
+++ .eleventy.js
@@ -15,20 +15,29 @@ module.exports = function (eleventyConfig) {
     }
 
     options.outputDir = path.join(__dirname, 'build', 'img')
+    options.widths = [256, 512, 1024, null]
+    options.cacheOptions = {
+
+    }
     const stats = await Image(path.join(__dirname, 'src', src), options)
     const lowestSrc = stats.jpeg[0]
-    const sizes = '100vw'
+    const sizes = '(max-width: 1024px) 100vw, 1024px'
 
     // Iterate over formats and widths
-    return `<picture>
+    return `
+    <span style="position: relative; display: block; margin-left: auto; margin-right: auto; max-width: 1024px; ">
+    <picture>
       ${Object.values(stats).map(imageFormat => {
       return `  <source type="image/${imageFormat[0].format}" srcset="${imageFormat.map(entry => `${entry.url} ${entry.width}w`).join(', ')}" sizes="${sizes}">`
     }).join('\n')}
-        <img
-          alt="${alt}"
-          src="${lowestSrc.url}"
-          width="${lowestSrc.width}"
-          height="${lowestSrc.height}">
-      </picture>`
+      <img
+        loading="lazy"
+        style="max-width: 100%; width: 100%; margin: 0px; vertical-align: middle;"
+        alt="${alt}"
+        src="${lowestSrc.url}"
+        width="${lowestSrc.width}"
+        height="${lowestSrc.height}">
+    </picture>
+    </span>`
   })
 }
