6aaadd04f8255668fec461a2a213b94c46fa8d87
Luciano <lucianomammino@gmail.com>
2020-09-20 21:10:04 +0100

    Fixed casing in latest quote

--- src/_data/quotes.js
+++ src/_data/quotes.js
@@ -54,7 +54,7 @@ module.exports = [
     name: 'Mike Alche',
     role: 'Node.js and React consultant',
     picture: './img/people/mike-alche-node-js.jpg',
-    text: 'Every time a new Node.JS book comes out I read it. I think there isn’t a single one that I haven’t read. And out of all of them I must say that Node.JS Design Patterns is by far — and I mean it BY FAR— the undisputed best. To put it in the most honest way possible: this is the book I go to read when preparing to teach a Node.js class to groups of software engineers.'
+    text: 'Every time a new Node.js book comes out I read it. I think there isn’t a single one that I haven’t read. And out of all of them I must say that Node.js Design Patterns is by far — and I mean it BY FAR— the undisputed best. To put it in the most honest way possible: this is the book I go to read when preparing to teach a Node.js class to groups of software engineers.'
   }
   // ,{
   //   name: 'Yan Cui',
