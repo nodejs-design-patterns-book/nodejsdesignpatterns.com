81d638c1138abb5d88770b011d4d0fc30d9b5533
Luciano Mammino <lucianomammino@gmail.com>
2024-01-02 13:46:44 +0100

    Working on new copy - part 2

--- src/_data/quotes.js
+++ src/_data/quotes.js
@@ -1,86 +1,86 @@
 const quotes = [
   {
     name: 'Joe Karlsson',
-    role: 'Software Engineer at MongoDB',
+    role: 'Developer Advocate at Tinybird',
     picture: './img/people/joe-karlsson-node-js.jpg',
     text: `I've read basically every Node.js book ever published, and this is my personal favorite (and best) by far. Even months after reading all the way through this text, I still learn new things each time I browse through it.
     What can I say, this is a must for anyone writing code in Node. js. Everything in the book, including the code examples, are very useful in practice.`
   },
   {
     name: 'Gleb Bahmutov',
-    role: 'VP of Engineering at Cypress.io',
+    role: 'Senior Director Of Engineering at Mercari',
     picture: './img/people/gleb-bahmutov-node-js.jpg',
     text: 'Wow! This book ... is amazing. After many years of programming with JavaScript, I still have learnt so much  from this book. It covers many topics relevant to large Node.js applications with ease, with many relevant code snippets making the material easily approachable. While not for beginners, it is a great book for anyone looking to take their JS applications to the higher level.'
   },
   {
     name: 'Radoslav Stankov',
-    role: 'Head of engineering at ProductHunt',
+    role: 'CTO and Co-Founder at Angry Building',
     picture: './img/people/radoslav-stankov-node-js.jpg',
     text: 'Node.js Design Patterns is an excellent resource for learning. I learned a lot of things that I thought I already knew. My favorite part is the Commonjs and ES6 import systems and a lot of the more advanced recipes.'
   },
   {
     name: 'Ire Aderinokun',
-    role: 'VP Engineering at BuyCoin Africa',
+    role: 'Frontend Engineer, Entrepreneur, Investor',
     picture: './img/people/ire-aderinokun-node-js.jpg',
     text: 'Node.js Design Patterns is a really in-depth look into the world of Node.js. Everything you ever wanted to know, and more! It’s very comprehensive, and gets into the details of how Node.js works. It’s a perfect book for anyone who already knows a bit about Node.js, and is looking to deepen their knowledge.'
   },
   {
     name: 'Ersel Aker',
-    role: 'Co-founder & CTO at JustFulfil',
+    role: 'Co-Founder at futurecast.studio',
     picture: './img/people/ersel-aker-node-js.jpg',
     text: 'Read through a few chapters of Node.js Design Patterns this morning. It\'s an excellent reference book covering everything from from the intricacies of the Node.js runtime to scaling your services to serve millions of users. Grabbed another copy for our engineering team to use as a study tool.'
   },
   {
     name: 'Mike Rourke',
-    role: 'Software Engineer and Author',
+    role: 'Software Engineer at a.i. solutions and Author',
     picture: './img/people/mike-rourke-node.jpg',
     text: 'There is a lot of useful content in this book, and I think the title doesn\'t do it justice. If you want a comprehensive book about Node.js that not only acts as a reference for best practices and design patterns, but also as a one-stop shop for understanding how Node.js works behind the scenes, look no further!'
   },
   {
     name: 'Maya Shavin',
-    role: 'Senior Frontend Engineer at Cloudinary',
+    role: 'Senior Software Engineer at Microsoft and Author',
     picture: './img/people/maya-shavin-node-js.jpg',
     text: 'This book surprised me for how detailed it is! It contains a lot of useful and easy-to-understand information. For someone familiar with the Node.js, I\'m intrigued by how much depth the book provides in working with Node.js. Every topic is approached in a very straightforward way: while being practical, this book offers different readers\' levels - from the most juniors to the experienced ones - an excellent reference to apply design patterns in any real scale Node.js application. Totally going to bookmark it as my favorite Node.js book!'
   },
   {
     name: 'Kostas Bariotis',
-    role: 'Software Engineer at Bulb',
+    role: 'Sr. Software Engineer at Mixmax',
     picture: './img/people/kostas-bariotis-node-js.jpg',
     text: 'Node.js Design Patterns is an amazing resource for every level of Node.js developers. It covers important design patters that are used in the Node.js world (modules, callbacks, asynchronicity, etc) and explores critical bits of the core library (promises, streams, event emitters, etc). Finally, it provides best practices on running Node.js as part of a bigger architecture where you need to horizontally scale an application, balance the incoming load and setup a message bus in between your services.'
   },
   {
     name: 'David Wells',
-    role: 'Serverless architect & full-stack developer',
+    role: 'Full Stack Engineer at Vendia',
     picture: './img/people/david-wells-node-js.jpg',
     text: 'If you want a deep dive into how Node.js works and the available design patterns, I\'d recommend checking out Mario and Luciano\'s new book. I particularly enjoyed the section on using streams. After reading, I realized haven\'t been leveraging streams enough in my projects.'
   },
   {
     name: 'Simon Høiberg',
-    role: 'Freelance Software Engineer',
+    role: 'Software Developer and Startup CEO',
     picture: './img/people/simon-hoiberg-node-js.jpg',
     text: 'Node.js Design Patterns is a great, in-depth resource for both beginners and advanced. Even though I\'ve been writing Node.js applications for the past 5 years, I learned a lot from this comprehensive piece of work. By far the best Node.js book currently out there.'
   },
   {
     name: 'Mike Alche',
-    role: 'Node.js and React consultant',
+    role: 'React, React Native & Node.js software consultant',
     picture: './img/people/mike-alche-node-js.jpg',
     text: 'Every time a new Node.js book comes out I read it. I think there isn’t a single one that I haven’t read. And out of all of them I must say that Node.js Design Patterns is by far — and I mean it BY FAR— the undisputed best. To put it in the most honest way possible: this is the book I go to read when preparing to teach a Node.js class to groups of software engineers.'
   },
   {
     name: 'Theodore Vorillas',
-    role: 'JavaScript Engineer at Workable',
+    role: 'Independent Software Engineer and JavaScript expert',
     picture: './img/people/theodore-vorillas-node-js.jpg',
     text: 'Node.js Design Patterns is a must read, an excellent resource for learning how to build Node.js applications. I loved the fact that you can actually learn new stuff regardless your experience. My favorite chapters are about scaling a production application (Universal JavaScript, Scalability and Architectural Patterns, Advanced Recipes)... and the coding samples are also available for free on GitHub!'
   },
   {
     name: 'David Gonzalez',
-    role: 'Full stack engineer/DevOps, Google Developer Expert and author',
+    role: 'Principal Engineer at Cloudsmith and author',
     picture: './img/people/david-gonzalez-node-js.jpg',
     text: 'I am a hardcore techie, big into Node.js and I have to say I have learnt a lot by just going through Node.js Design Patterns. The level of detail and quality of the examples are incredible and in general, it is a good book have as a reference to solve any problem that might arise while code (not only in Node.js). This is a book I recommend to my students to go from 0 to hero and keep it as a reference forever. The previous edition was good, but this one is just out of this world.'
   },
   {
     name: 'Maxim Salnikov',
-    role: 'Developer Engagement Lead at Microsoft',
+    role: 'Developer Productivity Business Lead at Microsoft',
     picture: './img/people/maxim-salnikov-node-js.jpg',
     text: 'I have "Node.js Design Patterns" always at hand both when I start quick new projects and when the time comes to refactor the larger and mature ones. It\'s an invaluable and well-structured source of patterns, best practices, and guidelines.  Also, it’s always good to know that particular features are in use in well-known projects - thanks to the “In the wild” section. As a full-stack developer focused on the front-end, I especially enjoyed a chapter about universal JavaScript.'
   }
