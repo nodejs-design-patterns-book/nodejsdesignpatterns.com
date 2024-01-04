8137e576fdd1bd5a938a490f81cb769cf619eb9f
Luciano Mammino <lucianomammino@gmail.com>
2024-01-04 09:38:37 +0000

    New copy (#19)
    * Working on new copu
    
    * Working on new copy - part 2
    
    * Completed rewrite
    
    * fixed broken tag and inconsistent list style
    
    * CSS improvements and minor cleanups
    
    * Applied first batch of suggestions from Mario
    
    * Apply suggestions from code review
    
    Co-authored-by: Mario Casciaro <mariocasciaro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mario Casciaro <mariocasciaro@users.noreply.github.com>

---
+++ src/_data/reviews.js
@@ -0,0 +1,60 @@
+const reviews = [
+  {
+    name: 'Rebecca',
+    link: 'https://www.amazon.com/gp/customer-reviews/R1BDT82UPKFUPZ/',
+    rating: 5,
+    title: 'Extremely helpful',
+    text: 'From the title, the book sounded like it would be exactly what I needed and I am happy to say it is. There are always several ways of doing things and the pitfalls/best practices (and reasons behind them) are not always immediately obvious. This book gives you the tools to reason about which pattern is going to be best for a certain task, which is so helpful. I really appreciate the quality of the resource. They also provide exercises at the end of the chapter which I think is super helpful for reinforcing concepts and keeping things interactive.'
+  },
+  {
+    name: 'James Ma',
+    link: 'https://www.amazon.com/gp/customer-reviews/RM0UKVO4TDJMQ/',
+    rating: 5,
+    title: 'Highly recommend to add this to your toolkit if you\'re developing Node.js applications',
+    text: 'I\'ve been developing for Node.js for a number of years, and this book helps me to understand concepts and things under the hood I’ve never had a chance to explore, all in a single source. I’m amazed that design patterns are covered with JS codes, as it shows me how to design and architect Node applications better to run at scale. Another thing I love is the hands-on aspects in guiding me to set up and run production-grade services such as task distribution, callbacks, concurrency and streams. Highly recommend to add this to your toolkit if you\'re developing Node.js applications.'
+  },
+  {
+    name: 'Valerio',
+    link: 'https://www.amazon.com/gp/customer-reviews/R236KX8KO84CBX/',
+    rating: 5,
+    title: 'Clean and well structured',
+    text: 'This book gets straight to the point, and there are many practical examples. You can find fundamentals and more advanced notions. Each chapter is well designed and the presentation of topics follows a well-structured logic.'
+  },
+  {
+    name: 'Or Hasson',
+    link: 'https://www.amazon.com/gp/customer-reviews/R34MMCIN98JI43/',
+    rating: 5,
+    title: 'Deep knowledge about Node.js',
+    text: 'I found this book very useful and knowledgeable. Great and clean up to date content. You can either read it in order or use it as a reference. Love it. Thank you'
+  },
+  {
+    name: 'Renan Truppel Ayoub',
+    link: 'https://www.amazon.com.br/gp/customer-reviews/R3TAC35N4GE0Z0/',
+    rating: 5,
+    title: 'Excelent content',
+    text: 'I am liking the book, it explains a lot of low level concepts of Node.js and the part of design patterns shows great exemples how to use the pattern explained, instead of just theory. Worth the purchase. I really recommend.'
+  },
+  {
+    name: 'Massimiliano C.',
+    link: 'https://www.amazon.it/gp/customer-reviews/R1UD3EH7PB4YRF/',
+    rating: 5,
+    title: 'Wonderful book, the best for Node.js. A book to have in your library',
+    text: 'A really well structured book, with clear and precise examples, aimed at explaining the concepts set out. Very complete and comprehensive, it covers practically all aspects and approaches of Node. Great Job for Mario and Luciano.'
+  },
+  {
+    name: 'Juan',
+    link: 'https://www.amazon.es/gp/customer-reviews/R2Q4HJEO6JDVTR/',
+    rating: 5,
+    title: 'Indispensable',
+    text: 'A book that I always recommend to junior developers or those who want to improve their skills with Node. Well structured and with the necessary content to understand how to work professionally with this language.'
+  },
+  {
+    name: 'NeoModulus',
+    link: 'https://www.amazon.com/gp/customer-reviews/R3DOZFPJ6EWFRL/',
+    rating: 5,
+    title: 'Deep dive into Node.js, JavaScript style',
+    text: 'An excellent book that covers many topics, including design patterns, with every example in JavaScript. The author does a great job of explaining each subject with multiple examples refactoring each into the optimal solution.'
+  }
+]
+
+export default reviews
