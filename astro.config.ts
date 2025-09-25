6ff774b7a4eaf2f5ea1c91120c7150d85d34d652
Luciano Mammino <lmammino@users.noreply.github.com>
2025-09-25 08:02:05 +0200

    feat: Added Google Analytics

--- astro.config.ts
+++ astro.config.ts
@@ -5,11 +5,17 @@ import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig, fontProviders } from 'astro/config'
 import expressiveCode from 'astro-expressive-code'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nodejsdesignpatterns.com',
-  integrations: [react(), expressiveCode(), sitemap()],
+  integrations: [
+    react(),
+    expressiveCode(),
+    sitemap(),
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
